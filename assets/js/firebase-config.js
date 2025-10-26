db8cd80730473f744c04099cd4c213d6044ad37c
Dilanjan DK <96403575+DilanjanDK7@users.noreply.github.com>
2025-10-26 16:41:42 -0400

    Add Firebase Live

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,8 +1,4 @@
 
-// DO NOT COMMIT REAL KEYS HERE. Use firebase-config.example.js as a template.
-// This file intentionally left without secrets.
-window.FIREBASE_CONFIG = undefined;
-
 window.FIREBASE_CONFIG = {
     apiKey: "AIzaSyB5srCg2IxMzJPFChGfGbQ8b7QRlx_pOHQ",
     authDomain: "dkwebsite-12f83.firebaseapp.com",
@@ -10,7 +6,7 @@ window.FIREBASE_CONFIG = {
     storageBucket: "dkwebsite-12f83.firebasestorage.app",
     messagingSenderId: "668179593495",
     appId: "1:668179593495:web:0e9cc223693f9f87cb1f52",
-    measurementId: "G-NSWJLLH016
+    measurementId: "G-NSWJLLH016"
   };
   
   
