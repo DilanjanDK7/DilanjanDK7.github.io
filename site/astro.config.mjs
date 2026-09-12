17ea0690a618dbcdea41ef4395df85ef853ce039
Dilanjan DK <96403575+DilanjanDK7@users.noreply.github.com>
2026-09-12 19:23:34 -0400

    fix: update GitHub Actions to use latest artifact upload and deployment actions

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://dilanjandk7.github.io',
+  site: 'https://www.dilanjandk.com',
   base: '/',
   output: 'static',
   outDir: '../dist',
